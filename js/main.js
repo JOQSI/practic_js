36c23f2c52088b4f476552997308f9c7518b5e4b
vinda <vindanill07@gmail.com>
2025-02-26 23:40:41 +0700

    сделал все до 2 и ее тоже

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let app = new Vue ({
+    el: '#app',
+    data: {
+        product: "Socks",
+        description: "A pair of warm, fuzzy socks",
+        product: "Носки",
+        description: "Пара теплых, пушистых носков.",
+        image: "./img/Green-Socks.jpg",
+        altText: "Пара носков",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+    }
+
+})
