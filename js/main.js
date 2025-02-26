2945af65b8b36e49697aa0b8b22bb4b21ac6a473
vinda <vindanill07@gmail.com>
2025-02-26 23:51:31 +0700

    3 практическая

--- js/main.js
+++ js/main.js
@@ -7,7 +7,12 @@ let app = new Vue ({
         description: "Пара теплых, пушистых носков.",
         image: "./img/Green-Socks.jpg",
         altText: "Пара носков",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inStock: true,
+        inventory: 100,
+        onSale: true
     }
 
 })
